eb27d0579524cdc32117c8724338a871e08c0fec
FJB-ouweDremelaar <ferrie.bank@gmail.com>
2020-06-06 17:09:59 +0200

    added ROME feed library

--- src/main/java/com/fjbproductions/scrapert/NOSController.java
+++ src/main/java/com/fjbproductions/scrapert/NOSController.java
@@ -1,13 +1,23 @@
 package com.fjbproductions.scrapert;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NOSController {
 
+    @Autowired
+    private NOSService service;
+
     @RequestMapping("/test")
     public String testMessage() {
         return "This is the NOSController, bitches!";
+    }
+
+    @RequestMapping("/nos")
+    public String getMessage() {
+
+        return service.getCurrent();
     }
 }
--- src/main/java/com/fjbproductions/scrapert/NOSService.java
+++ src/main/java/com/fjbproductions/scrapert/NOSService.java
@@ -1,4 +1,46 @@
 package com.fjbproductions.scrapert;
 
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+import org.springframework.stereotype.Service;
+
+import java.net.URL;
+
+@Service
 public class NOSService {
+
+    private static String feedURL = "http://feeds.nos.nl/nosnieuwsalgemeen";
+
+    public String getCurrent() {
+
+        String result = "nothing!";
+
+        try {
+
+            try (XmlReader reader = new XmlReader(new URL(feedURL))) {
+
+                SyndFeed feed = new SyndFeedInput().build(reader);
+                System.out.println(feed.getTitle());
+                System.out.println("***********************************");
+
+                result = "";
+                for (SyndEntry entry : feed.getEntries()) {
+
+                    result += entry.getTitle() + " ** ";
+
+                    System.out.println(entry);
+                    System.out.println("***********************************");
+                }
+
+                System.out.println("Done");
+            }
+
+        }  catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
 }
