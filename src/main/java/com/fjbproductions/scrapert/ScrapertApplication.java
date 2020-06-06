80c9a6050ddc7a87159e86079d9b8df801053079
FJB-ouweDremelaar <ferrie.bank@gmail.com>
2020-06-06 16:07:47 +0200

    added basic project structure from Spring Initializr

---
+++ src/main/java/com/fjbproductions/scrapert/ScrapertApplication.java
@@ -0,0 +1,13 @@
+package com.fjbproductions.scrapert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fjbproductions/scrapert/ScrapertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjbproductions.scrapert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
